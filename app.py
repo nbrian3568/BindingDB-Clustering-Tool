639214a0edbb1f3fe38e796a225562695a42483b
nbrian3568 <NBrian3568@gmail.com>
2025-06-05 12:09:06 -0700

    app.py

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def serve_processed_file(uniprot_id, filename):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=False)
